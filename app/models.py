eea8608d1a0bb2584a7a46149d1e58a91d82b441
Jason Kerr <jrick.kerr@yahoo.com>
2019-04-24 13:56:28 -0500

    DATABASE

--- app/models.py
+++ app/models.py
@@ -5,9 +5,9 @@ from werkzeug.security import generate_password_hash
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(255), default= "Something")
-    first_name = db.Column(db.String(80), default= "Something")
-    last_name = db.Column(db.String(80), default= "Something")
+    password = db.Column(db.String(255),nullable = False )
+    first_name = db.Column(db.String(80),)
+    last_name = db.Column(db.String(80),)
     email= db.Column(db.String(20), default= "Something")
     location= db.Column(db.String(60), default= "Something")
     biography= db.Column(db.Text,nullable = False, default= "Something")
@@ -15,13 +15,20 @@ class User(db.Model):
     joined_on = db.Column(db.String(20), default= "Something")
     post = db.relationship("Post", backref= "author", lazy =True)
     like = db.relationship("Likes", backref = "liker")
-    
+    follower = db.relationship("Follow",foreign_keys="Follow.user_id",backref= "followeee")
+    followee = db.relationship("Follow", foreign_keys="Follow.follower_id", backref = "folowereee")
+   
 
-
-    def __init__(self,first_name,last_name,username):
-        self.first_name=first_name
-        self.last_name=last_name
-        self.username=username
+    def __init__(self, username, password, first_name, last_name, email, location, biography, profile_photo, joined_on):
+        self.username = username
+        self.password = generate_password_hash(password)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.location = location
+        self.biography = biography
+        self.profile_photo = profile_photo
+        self.joined_on = joined_on
         
 
 
@@ -51,10 +58,29 @@ class Post(db.Model):
     created_on = db.Column(db.String(20))
     likes = db.relationship('Likes',backref="Likers")
 
+    def __init__(self,user_id,photo,caption,created_on):
+        self.user_id = user_id
+        self.photo = photo
+        self.caption = caption
+        self.created_on = created_on
+    
 class Likes(db.Model):
     id= db.Column(db.Integer,primary_key = True)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
     post_id = db.Column(db.Integer,db.ForeignKey('post.id'))
 
-##class Follows(db.Model):
-  #####follower_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __init__(self,post_id,user_id):
+        self.user_id = user_id
+        self.post_id = post_id
+
+
+
+class Follow(db.Model):
+    id= db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    follower_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self,user_id,follower_id):
+        self.user_id= user_id
+        self.follower_id = follower_id
+        
